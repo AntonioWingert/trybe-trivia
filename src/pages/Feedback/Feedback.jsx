7d87b69fa66e75aafc5db172e2e958a7bdb5f6ee
Thiago Lira <thiago.slira@outlook.com>
2023-01-12 18:46:02 -0300

    Feita a renderização condicional das mensagens de feedback

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { string } from 'prop-types';
+import { number, string } from 'prop-types';
+
+const THREE = 3;
 
 class Feedback extends Component {
   render() {
-    const { gravatarUser } = this.props;
+    const { gravatarUser, assertions, score } = this.props;
     const urlGravatar = `https://www.gravatar.com/avatar/${gravatarUser}`;
     return (
       <div>
@@ -13,18 +15,26 @@ class Feedback extends Component {
           data-testid="header-profile-picture"
           alt="avatar-gravatar"
         />
-        <p data-testid="feedback-text">Feedback</p>
+        {assertions < THREE ? (
+          <h3 data-testid="feedback-text">Could be better...</h3>)
+          : (<h3 data-testid="feedback-text">Well Done!</h3>)}
+        <p>{`Você acertou ${assertions} questões!`}</p>
+        <p>{`Um total de ${score} pontos`}</p>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ player: { gravatarEmail } }) => ({
+const mapStateToProps = ({ player: { gravatarEmail, assertions, score } }) => ({
   gravatarUser: gravatarEmail,
+  assertions,
+  score,
 });
 
 Feedback.propTypes = {
   gravatarUser: string.isRequired,
+  assertions: number.isRequired,
+  score: number.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);

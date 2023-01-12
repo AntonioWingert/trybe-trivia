3ded9dbe07c309c6871d22159b7d7a3af81e0b28
Phyllipesa <phyllipe.sjc@hotmail.com>
2023-01-12 19:33:28 -0300

    Requisitos 15 e 16 concluidos

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Feedback extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div>
+        <div data-testid="feedback-text">Feedback</div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+        >
+          <Link to="/">Play Again</Link>
+        </button>
+
+        <button
+          type="button"
+          data-testid="btn-ranking"
+        >
+          <Link to="/ranking">Ranking</Link>
+        </button>
+      </div>
     );
   }
 }

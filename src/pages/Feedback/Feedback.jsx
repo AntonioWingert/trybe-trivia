f9eb7528a995abcb9ca2ed99d45f403626591b60
Thiago Lira <thiago.slira@outlook.com>
2023-01-12 18:23:27 -0300

    VRenderizando imagem do usuário na page de Feedback

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { string } from 'prop-types';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const { gravatarUser } = this.props;
+    const urlGravatar = `https://www.gravatar.com/avatar/${gravatarUser}`;
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div>
+        <img
+          src={ urlGravatar }
+          data-testid="header-profile-picture"
+          alt="avatar-gravatar"
+        />
+        <p data-testid="feedback-text">Feedback</p>
+      </div>
     );
   }
 }
+
+const mapStateToProps = ({ player: { gravatarEmail } }) => ({
+  gravatarUser: gravatarEmail,
+});
+
+Feedback.propTypes = {
+  gravatarUser: string.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
